8829d3a07f97ebe46bff40adf7153ce57547f954
馥凝儒雅 <57082522+endless-waiting@users.noreply.github.com>
2024-09-18 14:46:22 +0800

    创建项目

---
+++ src/main/java/tvbox/TVBoxApplication.java
@@ -0,0 +1,11 @@
+package tvbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TVBoxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TVBoxApplication.class, args);
+    }
+}
